8092b1ae221ebbdbc78b9cc5301c6a1df36ae28f
nip0p0 <kn.n1p0p0@gmail.com>
2016-11-28 02:10:36 +0900

    Add status feed function

--- app/models/user.rb
+++ app/models/user.rb
@@ -70,17 +70,16 @@ class User < ActiveRecord::Base
     UserMailer.password_reset(self).deliver_now
   end
 
-  # パスワード再設定の期限が切れている場合はtrueを返す
+  # パスワードリセットが期限切れならtrueを返す
   def password_reset_expired?
     reset_sent_at < 2.hours.ago
   end
 
-  # 試作feedの定義
-  # 完全な実装は第12章「ユーザーをフォローする」を参照してください。
+  # ユーザーのステータスフィードを返す
   def feed
-    Micropost.where("user_id = ?", id)
+    following_ids = "SELECT followed_id FROM relationships WHERE  follower_id = :user_id"
+    Micropost.where("user_id IN (#{following_ids}) OR user_id = :user_id", user_id: id)
   end
-
 
   # ユーザーをフォローする
   def follow(other_user)
